d4d454bd7ac139ebee2a3614c541217109ae3f69
Bill1907 <a31829660@gamil.com>
2025-04-22 10:06:38 +0900

    Update .gitignore and modify greeting message in AppService
    - Added TODO.md, node_modules, and dist to .gitignore.
    - Updated greeting message in AppService to 'Hello Worldrum tongtong!'

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worldrum!';
+    return 'Hello Worldrum tongtong!';
   }
 }
